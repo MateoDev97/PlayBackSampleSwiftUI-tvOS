9360c579eff117dc6cae149bf8912458b8eca8f8
Brian Ortiz <mateo972000@gmail.com>
2023-03-31 08:30:24 -0400

    Initial Commit

---
+++ VideoPlayBack/VideoPlayBackApp.swift
@@ -0,0 +1,17 @@
+//
+//  VideoPlayBackApp.swift
+//  VideoPlayBack
+//
+//  Created by Brian Ortiz on 2023-03-31.
+//
+
+import SwiftUI
+
+@main
+struct VideoPlayBackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
